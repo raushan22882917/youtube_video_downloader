0919186c5b0c97c0ba404ef88ee7fc2a7ecab4bf
Raushan Kumar <121452343+raushan22882917@users.noreply.github.com>
2024-10-25 08:16:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,9 @@
 import os
 import yt_dlp
-import requests
 from datetime import datetime
 from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
-
-# Path to the cookies.txt file
-COOKIES_FILE = 'cookies.txt'  # Replace with the actual path
-
-# reCAPTCHA secret key (replace with your actual secret key)
-RECAPTCHA_SECRET_KEY = '3edqwfyutgkmjnhbgvrfedwsq'
-
-# Function to verify reCAPTCHA response
-def verify_recaptcha(response_token):
-    payload = {
-        'secret': RECAPTCHA_SECRET_KEY,
-        'response': response_token
-    }
-    recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify'
-    response = requests.post(recaptcha_url, data=payload)
-    result = response.json()
-    return result.get('success', False)
 
 @app.route('/')
 def index():
@@ -29,18 +11,12 @@ def index():
 
 @app.route('/count', methods=['POST'])
 def count_videos():
-    # Get the video URL and CAPTCHA response from the request
     video_url = request.json.get('url')
-    recaptcha_response = request.json.get('recaptcha')
-
-    # Verify reCAPTCHA response
-    if not verify_recaptcha(recaptcha_response):
-        return jsonify({'success': False, 'message': 'Invalid CAPTCHA'}), 400
 
     ydl_opts = {
         'quiet': True,
         'extract_flat': True,
-        'cookies': COOKIES_FILE,  # Use cookies to authenticate
+        'cookiefile': 'cookies.txt'  # Point to the cookies file
     }
 
     try:
@@ -56,14 +32,7 @@ def count_videos():
 
 @app.route('/download', methods=['POST'])
 def download_videos():
-    # Get the video URL and CAPTCHA response from the request
     video_url = request.json.get('url')
-    recaptcha_response = request.json.get('recaptcha')
-
-    # Verify reCAPTCHA response
-    if not verify_recaptcha(recaptcha_response):
-        return jsonify({'success': False, 'message': 'Invalid CAPTCHA'}), 400
-
     timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
     video_folder = os.path.join(os.path.expanduser('~'), 'Downloads', f'YouTube Downloads {timestamp}')
 
@@ -72,7 +41,7 @@ def download_videos():
 
     ydl_opts = {
         'outtmpl': os.path.join(video_folder, '%(title)s.%(ext)s'),
-        'cookies': COOKIES_FILE,  # Use cookies to authenticate
+        'cookiefile': 'cookies.txt'  # Point to the cookies file
     }
 
     try:
